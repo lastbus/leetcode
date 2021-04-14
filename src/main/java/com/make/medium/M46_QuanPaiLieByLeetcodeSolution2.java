e6cfea1c6bbd22ff018ca4f3e7fcb4235f256e33
lastbus <819307659@qq.com>
2021-04-14 22:22:50 +0800

    back track

---
+++ src/main/java/com/make/medium/M46_QuanPaiLieByLeetcodeSolution2.java
@@ -0,0 +1,41 @@
+package com.make.medium;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author make
+ * @create 2021-04-14
+ */
+public class M46_QuanPaiLieByLeetcodeSolution2 {
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        backtrack(nums.length, output, res, 0);
+
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+
+}
