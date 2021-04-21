cbd7d117f74c667a02afdff020133abdfd80bdd3
lastbus <819307659@qq.com>
2021-04-21 22:54:15 +0800

    jump game

---
+++ src/main/java/com/make/medium/M55_JumpGame.java
@@ -0,0 +1,25 @@
+package com.make.medium;
+
+/**
+ * @author make
+ * @create 2021-04-21
+ */
+public class M55_JumpGame {
+
+    public boolean canJump(int[] nums) {
+        for (int i = nums.length - 1; i >= 1; i--) {
+            if (!canJump(nums, i)) return false;
+        }
+        return true;
+    }
+
+    public boolean canJump(int[] nums, int end) {
+        for (int i = 1; end - i >= 0; i++) {
+            if (i <= nums[end - i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
