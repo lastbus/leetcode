57ddab8a21aec635b4dc9df5d9aa6146e9290725
lastbus <819307659@qq.com>
2021-04-21 23:04:00 +0800

    jump game

--- src/main/java/com/make/medium/M55_JumpGame.java
+++ src/main/java/com/make/medium/M55_JumpGame.java
@@ -22,4 +22,20 @@ public class M55_JumpGame {
         return false;
     }
 
+    // official solution, nicer
+    public boolean canJump2(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
