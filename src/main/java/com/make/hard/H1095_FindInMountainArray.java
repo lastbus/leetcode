352d276d71de2e09c2a6867f6206bc04f4768391
lastbus <819307659@qq.com>
2021-04-24 23:34:20 +0800

    binary search 2

--- src/main/java/com/make/hard/H1095_FindInMountainArray.java
+++ src/main/java/com/make/hard/H1095_FindInMountainArray.java
@@ -12,51 +12,51 @@ public class H1095_FindInMountainArray {
      }
 
      // my solution
-    public int findInMountainArray(int target, MountainArray mountainArr) {
-        int left = 0, n = mountainArr.length();
-        int right = n - 1;
-        int peak = -1;
-        while (left < right) {
-            int mid = (left + right) >> 1;
-            int midValue = mountainArr.get(mid);
-            if (mountainArr.get(mid - 1) < midValue && midValue > mountainArr.get(mid + 1)) {
-                peak = mid;
-                break;
-            }
-            if (midValue < mountainArr.get(mid + 1)) {
-                left = mid;
-            } else {
-                right = mid;
-            }
-        }
-        // search left side
-        left = 0;
-        right = peak;
-        while (left <= right) {
-            int mid = (left + right) >> 1;
-            int midValue = mountainArr.get(mid);
-            if (midValue == target) return mid;
-            if (mountainArr.get(left) <= target && midValue > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        // search right side
-        left = peak + 1;
-        right = n - 1;
-        while (left <= right) {
-            int mid = (left + right) >> 1;
-            int midValue = mountainArr.get(mid);
-            if (midValue == target) return mid;
-            if (mountainArr.get(left) >= target && midValue < target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return -1;
-    }
+     public int findInMountainArray(int target, MountainArray mountainArr) {
+         int left = 0, n = mountainArr.length();
+         int right = n - 1;
+         int peak = -1;
+         while (left < right) {
+             int mid = (left + right) >> 1;
+             int midValue = mountainArr.get(mid);
+             if (mountainArr.get(mid - 1) < midValue && midValue > mountainArr.get(mid + 1)) {
+                 peak = mid;
+                 break;
+             }
+             if (midValue < mountainArr.get(mid + 1)) {
+                 left = mid;
+             } else {
+                 right = mid;
+             }
+         }
+         // search left side
+         left = 0;
+         right = peak;
+         while (left <= right) {
+             int mid = (left + right) >> 1;
+             int midValue = mountainArr.get(mid);
+             if (midValue == target) return mid;
+             if (midValue > target) {
+                 right = mid - 1;
+             } else {
+                 left = mid + 1;
+             }
+         }
+         // search right side
+         left = peak + 1;
+         right = n - 1;
+         while (left <= right) {
+             int mid = (left + right) >> 1;
+             int midValue = mountainArr.get(mid);
+             if (midValue == target) return mid;
+             if (midValue < target) {
+                 right = mid - 1;
+             } else {
+                 left = mid + 1;
+             }
+         }
+         return -1;
+     }
 
 
 }
