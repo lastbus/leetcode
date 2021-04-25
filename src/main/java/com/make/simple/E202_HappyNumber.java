0b91a61e92b2b91490844402db89bca52c7b73df
lastbus <819307659@qq.com>
2021-04-25 22:10:28 +0800

    happy number

---
+++ src/main/java/com/make/simple/E202_HappyNumber.java
@@ -0,0 +1,32 @@
+package com.make.simple;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author make
+ * @create 2021-04-25
+ */
+public class E202_HappyNumber {
+
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+
+}
