9912c2fbef16305dc995f41e7329788dca61107c
lastbus <819307659@qq.com>
2021-04-26 22:54:56 +0800

    sqrt

---
+++ src/main/java/com/make/simple/E69_Sqrtx.java
@@ -0,0 +1,23 @@
+package com.make.simple;
+
+/**
+ * @author make
+ * @create 2021-04-26
+ */
+public class E69_Sqrtx {
+
+    public int mySqrt(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+}
