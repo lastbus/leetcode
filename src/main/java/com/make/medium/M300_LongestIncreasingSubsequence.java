561697a8897c4bcc14b993b79dee5a985c0ce501
lastbus <819307659@qq.com>
2021-04-27 23:06:50 +0800

    dp

---
+++ src/main/java/com/make/medium/M300_LongestIncreasingSubsequence.java
@@ -0,0 +1,28 @@
+package com.make.medium;
+
+/**
+ * @author make
+ * @create 2021-04-27
+ */
+public class M300_LongestIncreasingSubsequence {
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+
+}
