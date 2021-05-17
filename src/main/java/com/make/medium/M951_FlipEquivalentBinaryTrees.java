51861634b05a4f41cb953fadb13e57b32010b313
lastbus <819307659@qq.com>
2021-05-17 22:16:40 +0800

    tree

---
+++ src/main/java/com/make/medium/M951_FlipEquivalentBinaryTrees.java
@@ -0,0 +1,16 @@
+package com.make.medium;
+
+/**
+ * @author make
+ * @create 2021-05-17
+ */
+public class M951_FlipEquivalentBinaryTrees {
+
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == root2) return true;
+        if (root1 == null || root2 == null || root1.val != root2.val) return false;
+        return (flipEquiv(root1.left, root2.left) || flipEquiv(root1.left, root2.right)) &&
+                (flipEquiv(root1.right, root2.right) || flipEquiv(root1.right, root2.left));
+    }
+
+}
