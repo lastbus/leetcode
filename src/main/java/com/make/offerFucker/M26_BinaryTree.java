5303c317baff7aad6d6caf3fada3314718ce2202
lastbus <819307659@qq.com>
2021-05-19 22:46:46 +0800

    tree

---
+++ src/main/java/com/make/offerFucker/M26_BinaryTree.java
@@ -0,0 +1,17 @@
+package com.make.offerFucker;
+
+/**
+ * @author make
+ * @create 2021-05-19
+ */
+public class M26_BinaryTree {
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+    boolean recur(TreeNode A, TreeNode B) {
+        if(B == null) return true;
+        if(A == null || A.val != B.val) return false;
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+}
