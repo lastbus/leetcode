2615d9578e1edd89b75e7bd9eeeabfc09576117f
lastbus <819307659@qq.com>
2021-05-22 23:23:18 +0800

    median

--- src/main/java/com/make/medium/M475_Heaters.java
+++ src/main/java/com/make/medium/M475_Heaters.java
@@ -16,12 +16,40 @@ public class M475_Heaters {
     }
 
 
-    public int distance(int[] heaters, int pos) {
+    public int distance2(int[] heaters, int pos) {
         int min = Integer.MAX_VALUE;
         for (int heater : heaters) {
             min = Math.min(min, Math.abs(pos - heater));
         }
         return min;
+    }
+
+    public int distance(int[] heaters, int pos) {
+        int left = 0;
+        int right = heaters.length - 1;
+
+        int leftD = Math.abs(pos - heaters[left]);
+        int rightD = Math.abs(pos - heaters[right]);
+
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            int midD = Math.abs(pos - heaters[mid]);
+            if (midD <= rightD) {
+                right = mid;
+                rightD = Math.abs(pos - heaters[right]);
+            } else {
+                left = mid;
+                leftD = Math.abs(pos - heaters[left]);
+            }
+        }
+        return Math.abs(pos - heaters[left]);
+    }
+
+    public static void main(String[] args) {
+        M475_Heaters solution = new M475_Heaters();
+        int[] a =  {1, 2, 3, 4};
+        int[] b = {1, 4};
+        System.out.println(solution.findRadius(a, b));
     }
 
 }
