831ae99851295f62ec177e29682bc2d6f746561e
lastbus <819307659@qq.com>
2021-05-25 23:29:02 +0800

    substring

---
+++ src/main/java/com/make/simple/E7_ReverseInteger.java
@@ -0,0 +1,22 @@
+package com.make.simple;
+
+/**
+ * @author make
+ * @create 2021-05-25
+ */
+public class E7_ReverseInteger {
+
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            x /= 10;
+            rev = rev * 10 + digit;
+        }
+        return rev;
+    }
+
+}
