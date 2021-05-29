6385af718bdeca25ed971b5bdf406697ab47f252
lastbus <819307659@qq.com>
2021-05-29 15:14:28 +0800

    stack

--- src/main/java/com/make/simple/E20_ValidParentheses.java
+++ src/main/java/com/make/simple/E20_ValidParentheses.java
@@ -1,6 +1,6 @@
 package com.make.simple;
 
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @author make
@@ -45,5 +45,32 @@ public class E20_ValidParentheses {
         }
         return stack.isEmpty();
     }
+
+    public boolean isValid2(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<Character>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+
 
 }
