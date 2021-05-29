24013db4cb9a0db6ba39ce69ce2448c0b68d5888
lastbus <819307659@qq.com>
2021-05-29 14:58:52 +0800

    list remove

---
+++ src/main/java/com/make/medium/M19_RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,25 @@
+package com.make.medium;
+
+/**
+ * @author make
+ * @create 2021-05-29
+ */
+public class M19_RemoveNthNodeFromEndOfList {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; ++i) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+}
