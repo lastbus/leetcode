9e5f04d1e9b85af2db2d4c60503e181f2631b3e9
lastbus <819307659@qq.com>
2021-05-30 22:48:32 +0800

    binary search

---
+++ src/main/java/com/make/simple/E35_SearchInsertPosition.java
@@ -0,0 +1,23 @@
+package com.make.simple;
+
+/**
+ * @author make
+ * @create 2021-05-30
+ */
+public class E35_SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1
+        while (left <= right) {
+            int mid = (right + left) >> 1;
+            if (target == nums[mid]) return mid;
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+}
