930b2d24706fb48eeddade5d37da52dedc55fb60
lastbus <819307659@qq.com>
2021-06-01 22:44:45 +0800

    DP

---
+++ src/main/java/com/make/simple/E53_MaximumSubarray.java
@@ -0,0 +1,18 @@
+package com.make.simple;
+
+/**
+ * @author make
+ * @create 2021-06-01
+ */
+public class E53_MaximumSubarray {
+
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+}
