4d050185850d86676e4a11a70aa5cd51267a7b9a
lastbus <819307659@qq.com>
2021-06-03 22:59:02 +0800

    tree path sum

---
+++ src/main/java/com/make/simple/E112_PathSum.java
@@ -0,0 +1,41 @@
+package com.make.simple;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author make
+ * @create 2021-06-03
+ */
+public class E112_PathSum {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queNode = new LinkedList<TreeNode>();
+        Queue<Integer> queVal = new LinkedList<Integer>();
+        queNode.offer(root);
+        queVal.offer(root.val);
+        while (!queNode.isEmpty()) {
+            TreeNode now = queNode.poll();
+            int temp = queVal.poll();
+            if (now.left == null && now.right == null) {
+                if (temp == sum) {
+                    return true;
+                }
+                continue;
+            }
+            if (now.left != null) {
+                queNode.offer(now.left);
+                queVal.offer(now.left.val + temp);
+            }
+            if (now.right != null) {
+                queNode.offer(now.right);
+                queVal.offer(now.right.val + temp);
+            }
+        }
+        return false;
+    }
+
+}
