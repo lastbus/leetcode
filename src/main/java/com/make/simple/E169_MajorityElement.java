0a9d9309fb0f79ef156d32d53df691f8d46f7cdc
lastbus <819307659@qq.com>
2021-06-06 22:45:32 +0800

    nums

---
+++ src/main/java/com/make/simple/E169_MajorityElement.java
@@ -0,0 +1,23 @@
+package com.make.simple;
+
+/**
+ * @author make
+ * @create 2021-06-06
+ */
+public class E169_MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+}
