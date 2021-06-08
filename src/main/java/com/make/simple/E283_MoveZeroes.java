3da0790bf8dda4e0e02ea238dcf9e80090f46c0f
lastbus <819307659@qq.com>
2021-06-08 23:33:06 +0800

    array

---
+++ src/main/java/com/make/simple/E283_MoveZeroes.java
@@ -0,0 +1,26 @@
+package com.make.simple;
+
+/**
+ * @author make
+ * @create 2021-06-08
+ */
+public class E283_MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+
+}
